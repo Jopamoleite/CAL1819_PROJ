6473ed6ea13dd9cc119dece78bc146cd602d21f2
Pedro Esteves <up201705160@fe.up.pt>
2019-05-20 12:31:24 +0100

    Reading nodes

--- map.h
+++ map.h
@@ -11,6 +11,9 @@
 
 template<class T>
 Graph<T> readGraph(string name){
+    
+    Graph<VertexInfo> g;
+    
     string map_dir = "mapas/"+name+"/";
     ifstream f(map_dir+"T03_nodes_X_Y_"+name+".txt");
 
@@ -20,6 +23,32 @@ Graph<T> readGraph(string name){
     getline(f, file_line);
     lines = stoi(file_line);
 
+    int ID;
+    double X, Y;
+    
+    while(lines > 0){
+        getline(f, file_line);
+        file_line = file_line.substr(1);
+        
+        ID = stoi(file_line.substr(0, file_line.find(","));
+        file_line = file_line.substr(file_line.find(",")+1);
+
+        X = stod(file_line.substr(0, file_lind.find(",")));
+        file_line = file_line.substr(file_line.find(",")+1);
+
+        Y = stod(file_line.substr(0, file_line.find(")")));
+        
+        VertexInfo v(X, Y, ID);
+        
+        g.addVertex(v);
+        
+        lines--;
+    }
+
+    f.close();
+
+    f.open(map_dir+"T03_edges_"+name+".txt");
+    
     f.close();
 }
 
