a8ad4a9351df709eec3aa7ee83b407f77b7e2c73
Márcia Teixeira <marcia309@hotmail.com>
2019-05-20 12:16:47 +0100

    Added class with vertex info

---
+++ types.h
@@ -0,0 +1,25 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+class VertexInfo{
+    private:
+        double x;
+        double y;
+        double id;
+
+    public:
+        VertexInfo(double x, double y, double id){
+            this->x = x;
+            this->y = y;
+            this->id = id;
+        }
+
+        double getX(){return x;}
+
+        double getY(){return y;}
+
+        double getID(){return id;}
+
+};
+
+#endif
