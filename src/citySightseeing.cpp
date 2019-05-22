6859ccfbf9a56a4ac0fdcc5e061d3ab244ad746b
Pedro Esteves <up201705160@fe.up.pt>
2019-05-22 16:22:43 +0100

    Added citySightseeing files

---
+++ src/citySightseeing.cpp
@@ -0,0 +1,15 @@
+/*
+ * citySightseeing.cpp
+ */
+
+#include "citySightseeing.h"
+
+using namespace std;
+
+void firstIteration(VertexInfo start, vector<VertexInfo> pois, VertexInfo finish){
+
+}
+
+void secondIteration(VertexInfo start, vector<VertexInfo> pois, VertexInfo finish){
+
+}
---
+++ src/citySightseeing.h
@@ -0,0 +1,17 @@
+/*
+ * citySightseeing.h
+ */
+
+#ifndef SRC_CITYSIGHTSEEING_H_
+#define SRC_CITYSIGHTSEEING_H_
+
+#include <vector>
+
+#include "VertexInfo.h"
+
+void firstIteration(VertexInfo start, vector<VertexInfo> pois, VertexInfo finish);
+
+void secondIteration(VertexInfo start, vector<VertexInfo> pois, VertexInfo finish); //Caixeiro-viajante
+
+
+#endif /* SRC_CITYSIGHTSEEING_H_ */
