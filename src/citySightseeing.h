24ea6b36205473e39c7d8814b86be1c2825af7cb
Márcia Teixeira <marcia309@hotmail.com>
2019-05-23 13:57:41 +0100

    ups

--- src/citySightseeing.h
+++ src/citySightseeing.h
@@ -8,19 +8,12 @@
 #include <vector>
 
 #include "VertexInfo.h"
-<<<<<<< HEAD
 #include "Graph.h"
-=======
 #include "Person.h"
->>>>>>> 6bb9e3060698bb9a6fbb686823f2671fbd6bc199
 
 std::vector<std::vector<unsigned long>> dividePeople(std::vector<Person> people, int bus_capacity);
 
-<<<<<<< HEAD
 void firstIteration(Graph graph, VertexInfo start, vector<VertexInfo> pois, VertexInfo finish);
-=======
-void firstIteration(VertexInfo start, std::vector<VertexInfo> pois, VertexInfo finish);
->>>>>>> 6bb9e3060698bb9a6fbb686823f2671fbd6bc199
 
 void secondIteration(VertexInfo start, std::vector<VertexInfo> pois, VertexInfo finish); //Caixeiro-viajante
 
