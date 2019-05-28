6831409aa2cc47bd4056510a112819291fc340ab
João Leite <jopamoleite@gmail.com>
2019-05-28 12:27:51 +0100

    Fixed some errors

--- src/main.cpp
+++ src/main.cpp
@@ -159,52 +159,7 @@ int main() {
 						cout << "Added POI!\n";
 						break;
 					}
-					//TEST DIVIDE PEOPLE
-					/*vector<unsigned long> pois = {25504003, 1243857999};
-					vector<unsigned long> tmp_pois = {25504003, 1243857999, 25504005, 281726450, 25504011};
-					Person p(pois);
-					pois.clear();
-					pois = {25504003, 25504005};
-					Person p1(pois);
-					pois.clear();
-					pois = {25504003, 1243857999};
-					Person p2(pois);
-					pois.clear();
-					pois = {281726450, 1243857999};
-					Person p3(pois);
-					pois.clear();
-					pois = {281726450, 25504011};
-					Person p4(pois);
-					vector<Person> people = {p, p1, p2, p3, p4};
-					vector<vector<unsigned long>> div_pois = dividePeople(people, tmp_pois, 0);
-					for(size_t i = 0; i < div_pois.size(); i++){
-						cout << "Path: ";
-						for(size_t j = 0; j < div_pois[i].size(); j++){
-							cout << div_pois[i][j] << " ";
-						}
-						cout << endl;
-					}*/
 
-					//TEST DIJKSTA
-					/*VertexInfo v(25504003);
-					gv->setVertexColor(25504003, "blue");
-					VertexInfo v1(1243857999);
-					gv->setVertexColor(1243857999, "blue");
-					VertexInfo v2(25504005);
-					gv->setVertexColor(25504005, "blue");
-					VertexInfo v3(25504006);
-					gv->setVertexColor(25504006, "blue");
-					VertexInfo v4(281726450);
-					gv->setVertexColor(281726450, "blue");
-					VertexInfo v5(25504011);
-					gv->setVertexColor(25504011, "blue");
-					vector<VertexInfo> vec = {v1, v3, v2, v4};
-					vec = dijkstraShortestRoute(graph, v, vec, v5);
-					cout << vec.size() << endl;
-					for(size_t i = 0; i < vec.size(); i++){
-						if(!(vec[i] == v) && !(vec[i] == v1) && !(vec[i] == v2) && !(vec[i] == v3) && !(vec[i] == v4) && !(vec[i] == v5))
-							gv->setVertexColor(vec[i].getID(), "black");
-					}*/
 					leave = false;
 
 					for(unsigned int i = 0; i < temp_pois.size(); i++){
@@ -270,16 +225,32 @@ int main() {
 					break;
 				}
 				case 3:
+				{
+					vector<vector<unsigned long>> connectedPoi;
 					for(unsigned int i = 0; i < graph.getSCCs().size(); i++){
-						cout << "Connected POI:";
+						vector<unsigned long> v;
+						connectedPoi.push_back(v);
 						for(unsigned int j = 0; j < graph.getSCCs().at(i).size(); j++){
+							cout << j << endl;
 							VertexInfo v(graph.getSCCs().at(i).at(j));
 							if(graph.findVertex(v)->getInfo().getIsPOI())
-								cout << " " << graph.getSCCs().at(i).at(j);
+								connectedPoi.at(i).push_back(graph.getSCCs().at(i).at(j));
 						}
-						cout << endl;
+					}
+
+					for(unsigned int i = 0; i < connectedPoi.size(); i++){
+						if(connectedPoi.at(i).size()>1)
+							cout << "Connected POI:";
+						for(unsigned int j = 0; j < connectedPoi.at(i).size(); j++){
+							if(connectedPoi.at(i).size()>1)
+								cout << " " << connectedPoi.at(i).at(j);
+						}
+						if(connectedPoi.at(i).size()>1)
+							cout << endl;
 					}
 					break;
+
+				}
 				case 4:{
 					Person p(temp_pois);
 					//ADICIONAR A PESSOA A ALGUM LADO
@@ -301,7 +272,7 @@ int main() {
 				cout << "Graph doesn't exist yet!" << endl;
 				break;
 			}
-			graph.SCC();
+
 			for(unsigned int i = 0; i < graph.getSCCs().size(); i++){
 				for(unsigned int j = 0; j < graph.getSCCs().at(i).size(); j++){
 					gv->setVertexColor(graph.getSCCs().at(i).at(j), "red");
